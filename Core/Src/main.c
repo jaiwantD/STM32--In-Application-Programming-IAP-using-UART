7e0494dc9d869df857b03cce5870e96e2071f158
jaiwantD <jaiwantd.act2023@citchennai.net>
2026-05-23 14:41:55 +0530

    Fix: Debug LED

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -271,6 +271,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+      HAL_Delay(80);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
